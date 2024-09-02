b369a1e7532f70745253fe65d14137ffdd02a630
Reet Khanchandani <khanchandani.reet@gmail.com>
2024-09-02 12:54:18 -0700

    Reorganized project: Added both services and documentation

---
+++ Hello-service 3/src/main/java/com/example/Hello/service/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.Hello.service;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
+
---
+++ Hello-service 3/src/main/java/com/example/Hello/service/HelloServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.Hello.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
